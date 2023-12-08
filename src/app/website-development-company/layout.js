88c31182c684cd3c863b4953b32e0bf35e23ca43
dev-mr <m.rizwan@crystal-lite.net>
2023-12-09 02:29:03 +0500

    Initialize Project

---
+++ src/app/website-development-company/layout.js
@@ -0,0 +1,21 @@
+// Meta Data
+export const metadata = {
+    // Title & Descriptions
+    title: "Top Website Development Company | Creative Logo Designs",
+    description: "Creative Logo Designs: Your trusted website development company. Craft dynamic, responsive sites tailored to your needs. Unlock your digital potential. Reach out now!",
+    // Google Varification
+    verification: {
+        // google: 'ziIYRBahP4WcKcps6RWRDJvZ9A00pSoI39ByXDN14us',
+        other: {
+            "facebook-domain-verification": ['9mgotld2pcip5yvdv6yajdqx71xfvt'],
+        },
+    },
+    // Canonical
+    alternates: {
+        canonical: 'https://www.creativelogodesigns.io/website-development-company',
+    },
+}
+
+export default function RootLayout({ children }) {
+    return (children);
+}
